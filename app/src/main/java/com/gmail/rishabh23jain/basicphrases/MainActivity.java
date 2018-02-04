7c8d56fc174753cd06a4fd054206f397b6030048
Rishabh14997 <rishabh23jain@gmail.com>
2018-02-04 23:10:40 +0530

    Initial commit

---
+++ app/src/main/java/com/gmail/rishabh23jain/basicphrases/MainActivity.java
@@ -0,0 +1,26 @@
+package com.gmail.rishabh23jain.basicphrases;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void buttonTapped(View view)
+    {
+        int id=view.getId();
+        String ourId="";
+        ourId=view.getResources().getResourceEntryName(id);
+
+        int resourceId=getResources().getIdentifier(ourId,"raw","com.gmail.rishabh23jain.basicphrases");
+        MediaPlayer mplayer=MediaPlayer.create(this,resourceId);
+        mplayer.start();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
